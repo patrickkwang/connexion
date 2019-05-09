9aa1c211db6b5f63dff7431f70cbc0c1cf2778cf
patrickkwang <patrick@covar.com>
2019-05-09 11:07:22 -0400

    internalize OpenAPI specs
    from https://github.com/OAI/OpenAPI-Specification/tree/6d17b631fff35186c495b9e7d340222e19d60a71

--- connexion/spec.py
+++ connexion/spec.py
@@ -2,6 +2,7 @@ import abc
 import copy
 import json
 import pathlib
+import pkg_resources
 
 import jinja2
 import jsonschema
@@ -164,8 +165,8 @@ class Swagger2Specification(Specification):
     yaml_name = 'swagger.yaml'
     operation_cls = Swagger2Operation
 
-    response = requests.get('https://raw.githubusercontent.com/OAI/OpenAPI-Specification/6d17b631fff35186c495b9e7d340222e19d60a71/schemas/v2.0/schema.json')
-    openapi_schema = json.loads(response.text)
+    schema_string = pkg_resources.resource_string('connexion', 'resources/schemas/v2.0/schema.json')
+    openapi_schema = json.loads(schema_string.decode('utf-8'))
 
     @classmethod
     def _set_defaults(cls, spec):
@@ -221,8 +222,8 @@ class OpenAPISpecification(Specification):
     yaml_name = 'openapi.yaml'
     operation_cls = OpenAPIOperation
 
-    response = requests.get('https://raw.githubusercontent.com/OAI/OpenAPI-Specification/6d17b631fff35186c495b9e7d340222e19d60a71/schemas/v3.0/schema.json')
-    openapi_schema = json.loads(response.text)
+    schema_string = pkg_resources.resource_string('connexion', 'resources/schemas/v3.0/schema.json')
+    openapi_schema = json.loads(schema_string.decode('utf-8'))
 
     @classmethod
     def _set_defaults(cls, spec):
